cd094b7bd59204b0a1cd5b7137cda23ce1eae939
Lexi Spencer <deejaymadd@gmail.com>
2023-07-13 09:40:44 +1000

    updates

--- importfromURL.psm1
+++ importfromURL.psm1
@@ -27,7 +27,7 @@ function Import-FromURL {
         }
         if ($URL -match ".DLL" -or $DLL -eq $true) {
             try{
-                Write-Verbose "Importing DLL" -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
+                #Write-Verbose "Importing DLL" -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
                 Import-Module ([System.Reflection.Assembly]::Load((Invoke-WebRequest -UseBasicParsing -Uri $URL).content)) -ErrorAction SilentlyContinue > $null
             }catch{
                 Write-Error "Import-Module Failed to Import DLL, Make sure the Url is a direct link to the file."
@@ -36,7 +36,7 @@ function Import-FromURL {
         } elseif ($URL -match ".psm1" -or $Psm1 -eq $true) {
             try{
                 # Get module name without extension
-                Write-Verbose "Importing PowerShell Module" -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
+                #Write-Verbose "Importing PowerShell Module" -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
                 $ModuleFileNameComponents = (Split-Path $URL -Leaf).Split(".")
                 $ModuleName = (Split-Path $URL -Leaf).Replace(".$($ModuleFileNameComponents[$ModuleFileNameComponents.count-1])","")
                 if($Credential) {
@@ -44,23 +44,24 @@ function Import-FromURL {
 				} else {
                     $ImportedCode = (Invoke-WebRequest -Uri $URL -UseBasicParsing).Content
                 }
-                New-Module -Name "$($ModuleName)" -ScriptBlock ([Scriptblock]::Create($ImportedCode)) -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true) | Import-Module
-                #New-Module -Name "$($ModuleName)" -ScriptBlock {$ImportedCode.Content} -ErrorAction SilentlyContinue > $null
+                New-Module -Name "$($ModuleName)" -ScriptBlock ([Scriptblock]::Create($ImportedCode)) | Import-Module
+                New-Module -Name "$($ModuleName)" -ScriptBlock ([Scriptblock]::Create((Invoke-WebRequest -Uri $URL -UseBasicParsing -Credential $Credential).Content)) | Import-Module
+                #New-Module -Name "$($ModuleName)" -ScriptBlock ([Scriptblock]::Create((Invoke-WebRequest -Uri $URL -UseBasicParsing -Credential $Credential).Content)) -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true) | Import-Module
             }catch{
                 Write-Error "Import-Module Failed to Import Psm1 Module, Make sure the Url is a direct link to the raw code"
                 Write-Error $_
             }
         } elseif ($URL -match ".ps1" -or $Ps1 -eq $true) {
             try{
-                Write-Verbose "Importing PowerShell script" -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
+                #Write-Verbose "Importing PowerShell script" -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
                 if($Credential) {
 					$ImportedCode = (Invoke-WebRequest -Uri $URL -UseBasicParsing -Credential $Credential).Content
 				} else {
                     $ImportedCode = (Invoke-WebRequest -Uri $URL -UseBasicParsing).Content
                 }
                 Invoke-Command -ScriptBlock ([Scriptblock]::Create($ImportedCode))
-                Write-Verbose "Attempting to invoke the Ps1 script (There is no error handling for .Ps1 scripts)" -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
-                Write-Verbose "Windows Bug Tip: If a script prompts for UAC and then force closes the Session, Try running the session as admin" -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
+                #Write-Verbose "Attempting to invoke the Ps1 script (There is no error handling for .Ps1 scripts)" -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
+                #Write-Verbose "Windows Bug Tip: If a script prompts for UAC and then force closes the Session, Try running the session as admin" -Verbose:($PSCmdlet.MyInvocation.BoundParameters["Verbose"].IsPresent -eq $true)
             } catch {
                 Write-Error "Importing script failed, Make sure the Url is a direct link to the raw code"
                 Write-Error $_
